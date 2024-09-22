a56505a5c743298b2a11f0d3e032c4b68bc038bd
Beatriz Vieira <rvbeatriz73@gmail.com>
2024-09-22 00:25:09 -0300

    teste pageview

--- eventos.js
+++ eventos.js
@@ -1,7 +1,7 @@
 // Pageview
 window.dataLayer = window.dataLayer || [];
 window.dataLayer.push({
-  'event': 'pageview',
+  'event': 'page_view',
   'page_location': window.location.href,
   'page_path': window.location.pathname,
   'page_title': document.title
