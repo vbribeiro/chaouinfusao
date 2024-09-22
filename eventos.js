03701220518e952df01d3ea00b21ac31b97b76fa
Beatriz Vieira <rvbeatriz73@gmail.com>
2024-09-22 00:17:40 -0300

    adicionando eventos

---
+++ eventos.js
@@ -0,0 +1,8 @@
+// Pageview
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+  'event': 'pageview',
+  'page_location': window.location.href,
+  'page_path': window.location.pathname,
+  'page_title': document.title
+});
